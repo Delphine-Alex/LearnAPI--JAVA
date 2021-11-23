a5582d088eb76e808aa71f6eb72f24d6eb9d8d49
Delphine <delphine.pva@gmail.com>
2021-11-23 10:05:22 +0100

    start project

---
+++ src/main/java/com/ynov/productapl/ProductAplApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.productapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAplApplication.class, args);
+	}
+
+}
